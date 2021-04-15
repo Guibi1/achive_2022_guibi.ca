9dc54966c2b8677eff768152736aa0c4da3a609f
Laurent Stéphenne <guibi.ss999@gmail.com>
2021-04-15 16:16:36 -0400

    Removed unused import

--- src/components/Jeux/Consequences.js
+++ src/components/Jeux/Consequences.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import TextField from '@material-ui/core/TextField'
-import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
 
 import défis from "./défis.txt"
