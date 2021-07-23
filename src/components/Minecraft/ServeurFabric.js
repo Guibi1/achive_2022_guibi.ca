7931233dcc4b3e8009110afaa2bcf62a5b7296ba
Laurent Stéphenne <guibi.ss999@gmail.com>
2021-07-23 01:04:02 -0400

    Updated all Minecraft files

--- src/components/Minecraft/ServeurFabric.js
+++ src/components/Minecraft/ServeurFabric.js
@@ -75,28 +75,40 @@ class Minecraft extends Component
                             <div role="tabpanel" dir={'x'} style={{margin: "15px", minHeight: "220px"}}>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Installez GD Launcher depuis <a href="https://gdevs.io/" rel="noreferrer" target="_blank"> gdevs.io</a></span>
+                                    <span>Installez GD Launcher depuis <a href="https://gdevs.io/" rel="noreferrer" target="_blank">gdevs.io</a>;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Ajoutez une instance en cliquant sur le plus en bas à gauche de la fenêtre</span>
+                                    <span>Ajoutez une instance en cliquant sur le plus en bas à gauche de la fenêtre;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Naviguez jusqu'à l'onget d'ajout à partir d'un fichier .zip</span>
+                                    <span>Naviguez jusqu'à l'onget d'ajout à partir d'un fichier .zip;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Entrez l'adresse située ci-dessous</span>
+                                    <span>Entrez l'adresse située ci-dessous;</span>
                                 </span>
                                 <Button color="secondary" variant="outlined" onClick={() => this.handleCopy("https://guibi.ca/files/configServeurFabric.zip")} style={{margin: "10px"}}>Copier l'adresse</Button>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Appuyez sur la flèche en bas à droite jusqu'à ce que les téléchargements débutent</span>
+                                    <span>Appuyez sur la flèche en bas à droite jusqu'à ce que les téléchargements débutent;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Ouvrez Minecraft en cliquant sur l'instance une fois qu'elle est prête</span>
+                                    <span>Installez Java 16 depuis <a href="https://www.oracle.com/java/technologies/javase-jdk16-downloads.html" rel="noreferrer" target="_blank">oracle.com</a>;</span>
+                                </span>
+                                <span style={{display: 'flex', justifyContent: "center"}}>
+                                    <ArrowList fontSize="small"/>
+                                    <span>Attendez que l'instance ait fini de télécharger, puis appuyez dessus avec un clique droit et selectionnez "Manage";</span>
+                                </span>
+                                <span style={{display: 'flex', justifyContent: "center"}}>
+                                    <ArrowList fontSize="small"/>
+                                    <span>Cochez "Custom Java Path" et entrez y le chemin vers l'executable de Java 16. (ressemble à "C:\Program Files\Java\jdk-16.x.x\bin\java.exe");</span>
+                                </span>
+                                <span style={{display: 'flex', justifyContent: "center"}}>
+                                    <ArrowList fontSize="small"/>
+                                    <span>Ouvrez Minecraft en cliquant sur l'instance;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
@@ -106,20 +118,28 @@ class Minecraft extends Component
                             <div role="tabpanel" dir={'x'} style={{margin: "15px", minHeight: "220px"}}>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Installez Fabric 1.17 depuis <a href="https://fabricmc.net/use/" rel="noreferrer" target="_blank"> fabricmc.net</a></span>
+                                    <span>Installez Fabric 1.17 depuis <a href="https://fabricmc.net/use/" rel="noreferrer" target="_blank">fabricmc.net</a>;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Téléchargez les mods requis ci-dessous</span>
+                                    <span>Téléchargez les mods requis ci-dessous;</span>
                                 </span>
                                 <Button color="secondary" variant="outlined" onClick={this.handleDownload} style={{margin: "10px"}}>Télécharger les mods</Button>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Extractez les mods du fichier .zip dans le dossier ".minecraft/mods"</span>
+                                    <span>Extractez les mods du fichier .zip dans le dossier ".minecraft/mods";</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Ouvrez Minecraft Fabric 1.17 via votre launcher</span>
+                                    <span>Installez Java 16 depuis <a href="https://www.oracle.com/java/technologies/javase-jdk16-downloads.html" rel="noreferrer" target="_blank">oracle.com</a>;</span>
+                                </span>
+                                <span style={{display: 'flex', justifyContent: "center"}}>
+                                    <ArrowList fontSize="small"/>
+                                    <span>Modifiez le chemin du l'executable Java de votre launcher pour qu'il pointe sur l'executable de Java 16. (ressemble à "C:\Program Files\Java\jdk-16.x.x\bin\java.exe");</span>
+                                </span>
+                                <span style={{display: 'flex', justifyContent: "center"}}>
+                                    <ArrowList fontSize="small"/>
+                                    <span>Ouvrez Minecraft Fabric 1.17 via votre launcher;</span>
                                 </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
--- src/components/Minecraft/ServeurForge.js
+++ src/components/Minecraft/ServeurForge.js
@@ -77,56 +77,56 @@ class Minecraft extends Component
                             <div role="tabpanel" dir={'x'} style={{margin: "15px", minHeight: "220px"}}>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Installez GD Launcher depuis <a href="https://gdevs.io/" rel="noreferrer" target="_blank"> gdevs.io</a></span>
-                                </span>
+                                    <span>Installez GD Launcher depuis <a href="https://gdevs.io/" rel="noreferrer" target="_blank"> gdevs.io</a>;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Ajoutez une instance en cliquant sur le plus en bas à gauche de la fenêtre</span>
-                                </span>
+                                    <span>Ajoutez une instance en cliquant sur le plus en bas à gauche de la fenêtre;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Naviguez jusqu'à l'onget d'ajout à partir d'un fichier .zip</span>
-                                </span>
+                                    <span>Naviguez jusqu'à l'onget d'ajout à partir d'un fichier .zip;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Entrez l'adresse située ci-dessous</span>
-                                </span>
+                                    <span>Entrez l'adresse située ci-dessous;</span>
+                               </span>
                                 <Button color="secondary" variant="outlined" onClick={() => this.handleCopy("https://guibi.ca/files/configServeurForge.zip")} style={{margin: "10px"}}>Copier l'adresse</Button>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Appuyez sur la flèche en bas à droite jusqu'à ce que les téléchargements débutent</span>
-                                </span>
+                                    <span>Appuyez sur la flèche en bas à droite jusqu'à ce que les téléchargements débutent;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Ouvrez Minecraft en cliquant sur l'instance une fois qu'elle est prête</span>
-                                </span>
+                                    <span>Ouvrez Minecraft en cliquant sur l'instance une fois qu'elle est prête;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
                                     <span>Allez dans l'onglet Multijoueur et rejoignez notre serveur!</span>
-                                </span>
+                               </span>
                             </div>
                             <div role="tabpanel" dir={'x'} style={{margin: "15px", minHeight: "220px"}}>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Installez Forge depuis <a href="http://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.16.5.html" rel="noreferrer" target="_blank"> files.minecraftforge.net</a></span>
-                                </span>
+                                    <span>Installez Forge depuis <a href="http://files.minecraftforge.net/maven/net/minecraftforge/forge/index_1.16.5.html" rel="noreferrer" target="_blank"> files.minecraftforge.net</a>;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Téléchargez les mods requis ci-dessous</span>
-                                </span>
+                                    <span>Téléchargez les mods requis ci-dessous;</span>
+                               </span>
                                 <Button color="secondary" variant="outlined" onClick={this.handleDownload} style={{margin: "10px"}}>Télécharger les mods</Button>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Extractez les mods du fichier .zip dans le dossier "%appdata%/.minecraft/mods"</span>
-                                </span>
+                                    <span>Extractez les mods du fichier .zip dans le dossier "%appdata%/.minecraft/mods";</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
-                                    <span>Ouvrez Minecraft Forge 1.16.5 via votre launcher</span>
-                                </span>
+                                    <span>Ouvrez Minecraft Forge 1.16.5 via votre launcher;</span>
+                               </span>
                                 <span style={{display: 'flex', justifyContent: "center"}}>
                                     <ArrowList fontSize="small"/>
                                     <span>Allez dans l'onglet Multijoueur et ajoutez notre serveur avec l'adresse ci-dessous !</span>
-                                </span>
+                               </span>
                                 <Button color="secondary" variant="outlined" onClick={() => this.handleCopy("forge.minecraft.guibi.ca")} style={{margin: "10px"}}>Copier l'adresse</Button>
                             </div>
                         </SwipeableViews>
