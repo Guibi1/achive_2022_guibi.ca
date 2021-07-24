44174535a0584d70780d03c9883ec8f40249fd90
guibi.ss999@gmail.com <guibi.ss999@gmail.com>
2021-07-24 00:21:50 -0400

    Removed loadable on the entire app to remove flashing of NavBar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-
 import { ThemeProvider } from '@material-ui/styles'
-import CircularProgress from "@material-ui/core/CircularProgress"
-import loadable from '@loadable/component'
 
 import Theme from './Theme'
 import './style.css'
 
-const App = loadable(() => import('./App'))
+import App from './App'
 
 
-ReactDOM.render(<BrowserRouter><ThemeProvider theme={Theme()}><App fallback={<CircularProgress color="secondary"/>}/></ThemeProvider></BrowserRouter>, document.getElementById("root"))
+ReactDOM.render(<BrowserRouter><ThemeProvider theme={Theme()}><App/></ThemeProvider></BrowserRouter>, document.getElementById("root"))
